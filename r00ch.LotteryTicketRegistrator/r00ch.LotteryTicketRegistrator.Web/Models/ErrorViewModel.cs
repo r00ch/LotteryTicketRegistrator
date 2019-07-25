170ef50e44f00f561eb18921b403c3de3066fe7b
Zbysław Kowalski <z.kowalski@supremo.pl>
2019-07-25 10:23:20 +0200

    VS Project Init

---
+++ r00ch.LotteryTicketRegistrator/r00ch.LotteryTicketRegistrator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace r00ch.LotteryTicketRegistrator.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
